4ac2343df75078f07c9b10255ed8309ce332e405
zcrandall10 <zcrandall10@gmail.com>
2025-06-19 18:36:35 -0400

    telescope removed from init

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,9 @@
 local telescope = require 'telescope'
 local lga_actions = require 'telescope-live-grep-args.actions'
 telescope.setup {
+  -- You can put your default mappings / updates / etc. in here
+  --  All the info you're looking for is in `:help telescope.setup()`
+  --
   defaults = {
     mappings = {
       i = {
@@ -11,7 +14,11 @@ telescope.setup {
       },
     },
   },
+  -- pickers = {}
   extensions = {
+    ['ui-select'] = {
+      require('telescope.themes').get_dropdown(),
+    },
     live_grep_args = {
       auto_quoting = true,
       mappings = {
@@ -24,40 +31,42 @@ telescope.setup {
   },
 }
 
--- Enable telescope fzf native, if installed
+-- Enable Telescope extensions if they are installed
 pcall(require('telescope').load_extension, 'fzf')
-pcall(require('telescope').load_extension, 'live_grep_args')
+pcall(require('telescope').load_extension, 'ui-select')
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+local builtin = require 'telescope.builtin'
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+-- Slightly advanced example of overriding default behavior and theme
 vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+  -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-local builtin = require 'telescope.builtin'
-local search_all_files = function()
-  builtin.find_files { hidden = true, no_ignore = true }
-end
+-- It's also possible to pass additional configuration options.
+--  See `:help telescope.builtin.live_grep()` for information about particular keys
+vim.keymap.set('n', '<leader>s/', function()
+  builtin.live_grep {
+    grep_open_files = true,
+    prompt_title = 'Live Grep in Open Files',
+  }
+end, { desc = '[S]earch [/] in Open Files' })
 
-local lga_shortcuts = require 'telescope-live-grep-args.shortcuts'
-local grep_all = function()
-  lga_shortcuts.grep_visual_selection { postfix = ' --iglob **' }
-end
-
-vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', function()
-  builtin.find_files { hidden = true }
-end, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sa', search_all_files, { desc = '[S]earch [A]ll Files' })
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>fg', grep_all, { desc = 'Live Grep Args' })
-
-vim.keymap.set('n', '<leader>re', builtin.lsp_references, { desc = 'LSP References' })
+-- Shortcut for searching your Neovim configuration files
+vim.keymap.set('n', '<leader>sn', function()
+  builtin.find_files { cwd = vim.fn.stdpath 'config' }
+end, { desc = '[S]earch [N]eovim files' })
